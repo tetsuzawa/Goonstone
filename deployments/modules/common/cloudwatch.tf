27402230a41ade9a927d8c80fe4b397c874643b0
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-03-15 18:21:33 +0900

    Fix: ECR, Frontendのバグを修正

--- deployments/modules/common/cloudwatch.tf
+++ deployments/modules/common/cloudwatch.tf
@@ -1,7 +1,17 @@
-resource "aws_cloudwatch_log_group" "ecs" {
-  name = "/ecs/${var.name}"
+# Frontend
+resource "aws_cloudwatch_log_group" "frontend" {
+  name = "/ecs/${var.name}-frontend"
   tags = {
-    Name    = "${var.name}-ecs-logs"
+    Name    = "${var.name}-frontend-logs"
+    Product = var.name
+  }
+}
+
+# API
+resource "aws_cloudwatch_log_group" "api" {
+  name = "/ecs/${var.name}-api"
+  tags = {
+    Name    = "${var.name}-api-logs"
     Product = var.name
   }
 }
