5204761eed151bf8be167d12ea7f9b79bf980486
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-04-03 15:01:48 +0900

    Fix: プロパティ名を修正

--- containers/frontend/store/auth.js
+++ containers/frontend/store/auth.js
@@ -17,17 +17,17 @@ export const actions = {
   async register(context, data) {
     try {
       const response = await this.$axios.$post('/register', data);
-      context.commit('setUser', response.data)
+      context.commit('setUser', response.user)
     } catch (error) {
-      console.log(error);
+      console.log("error: login: %O", error);
     }
   },
   async login(context, data) {
     try {
       const response = await this.$axios.$post('/login', data);
-      context.commit('setUser', response.data)
+      context.commit('setUser', response.user)
     } catch (error) {
-      console.log(error);
+      console.log("error: login: %O", error);
     }
   },
   async logout(context) {
@@ -35,7 +35,16 @@ export const actions = {
       const response = await this.$axios.$post('/logout', null)
       context.commit('setUser', null)
     } catch (error) {
-      console.log(error);
+      console.log("error: logout: %O", error);
+    }
+  },
+  async currentUser(context) {
+    try {
+      const response = await this.$axios.$get('/user')
+      const user = response.user || null
+      context.commit('setUser', user)
+    } catch (error) {
+      console.log("error: logout: %O", error);
     }
   }
 };
